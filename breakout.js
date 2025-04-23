a5fa9333c0db7cbae7b9ec1691001b06c015df34
Agustin Abaroa <agustinabaroa@MacBook-Pro-de-Dr.local>
2025-04-23 07:59:09 -0700

    ASDF

--- breakout.js
+++ breakout.js
@@ -3,7 +3,7 @@ let cols = 10, fila = 4
 let niveles = 1
 let puntos = 0
 let vidas = 3
-let velBola = 5
+let velBola = 4
 let infoNivel
 
 
@@ -23,13 +23,14 @@ function setup() {
     bola.suelo()
     bola.mostrar()
 
-    for(let i = cuadros.length-1; i>=0; i--){
+    for(let i = cuadros.length - 1; i >= 0; i--) {
         cuadros[i].mostrar()
-        if(bola.golpe(cuadros[i])){
+        if (bola.golpe(cuadros[i])) {
             cuadros[i].golpe--
-            if(cuadros[i].golpe <= 0 && !cuadros[i].irrompible){
-                cuadros.splice(i,1)
+            if (cuadros[i].golpe <= 0 && !cuadros[i].irrompible) {
+                cuadros.splice(i, 1)
                 puntos++
+                bola.reverseY()
             }else{
                 bola.reverseY()
             }
@@ -39,50 +40,64 @@ function setup() {
 
     mostrarInfo()
 
-    if(cuadros.length === 0){
+    if (cuadros.length == 0) {
         niveles++
-        if(niveles <=3){
+        if (niveles <= 3) {
             cargaNivel(niveles)
             bola.reset()
         }else{
             noLoop()
-            textAlign(CENTER,CENTER)
+            textAlign(CENTER, CENTER)
             fill("red")
             textSize(24)
-            text("GANASTE!!", width/2, height/2)
+            text("QUE GUAPO ERES!!", width / 2, height / 2)
         }
     }
 
-    if(vidas <=0){
+    if (vidas <=0) {
         noLoop()
-        textAlign(CENTER,CENTER)
+        textAlign(CENTER, CENTER)
         fill("red")
         textSize(24)
-        text("PERDEDOR DEFINITIVO", width/2, height/2)
-        setTimeout(()=>{
+        text("PERDEDOR DEFINITIVO", width / 2, height / 2)
+        setTimeout(() => {
             reinicio()
-        },3000)
+        }, 3000)
     }
+
+    efectoRetro()
   }
 
-  function keyPressed(){
-    if(keyCode === LEFT_ARROW){
+  //funcion para el movimiento del jugador
+  function keyPressed() {
+    if (keyCode == LEFT_ARROW) {
         jugador.move(-1)
-    }else if(keyCode === RIGHT_ARROW){
+    }
+    if (keyCode == RIGHT_ARROW) {
         jugador.move(1)
     }
+    //adicionalmente se agrega un boton para que se lanze la pelota
+    if (key == ' ') {
+        if (!bola.lanzada) {
+            bola.xspeed = velBola * (random() < 0.5 ? 1 : -1)
+            bola.yspeed = -velBola
+            bola.lanzada = true
+        }
+    }
   }
 
-  function keyReleased(){
+  //funcion para que cuando no se presione ninguna tecla el jugador no se mueva
+  function keyReleased() {
     jugador.move(0)
   }
 
-  function mostrarInfo(){
-    fill("green")
+  //muestra a informacion del jugador
+  function mostrarInfo() {
+    fill(255, 255, 255)
     textSize(14)
-    text(`Putuacion: ${puntos}`, 10,20)
-    text(`Vidas: ${vidas}`, width-80,20)
-    text(`Nivel: ${niveles}`, width/2-20,20)
+    text(`Putuacion: ${puntos}`, 20, 20)
+    text(`Vidas: ${vidas}`, width - 80, 20)
+    text(`Nivel: ${niveles}`, width / 2 - 20, 20)
   }
 
   function cargaNivel(n){
@@ -91,26 +106,27 @@ function setup() {
     let w = width/cols
     let h = 20
 
-    for(let r = 0; r<infoNivel.length; r++){
-        for(let c = 0; c<infoNivel[r].length; c++){
+    for(let r = 0; r<infoNivel.length; r++) {
+        for(let c = 0; c<infoNivel[r].length; c++) {
             let valor = infoNivel[r][c]
-            if(valor !==0){
-                let x = c*w
-                let y = r*h+40
-                let golpe = valor === 2 ? 3 : 1
-                let irrompible = valor === 3
-                cuadros.push(new Cuadro(x,y,w,h,golpe,irrompible))
+            if (valor != 0) {
+                let x = c * w
+                let y = r * h + 40
+                let golpe = valor == 2 ? 3 : 1
+                let irrompible = valor == 3
+                cuadros.push(new Cuadro(x, y, w, h, golpe, irrompible))
             }
         }
     }
 
-    //aqui esta las velocidades del nivel 2 y 3 xD
-    if(n === 2) velBola = 7
-    if(n === 3) velBola = 10
-
+    //aqui esta las velocidades de los niveles
+    velBola = n + 4
+    //if (n == 2) velBola = 6
+    //if (n == 3) velBola = 7
   }
 
-  function reinicio(){
+  //funcion para reiniciar si se pierde
+  function reinicio() {
     vidas = 3
     puntos = 0
     niveles = 1
@@ -121,83 +137,92 @@ function setup() {
     loop()
   }
 
-  class Jugador{
-    constructor(){
-        this.w=100
-        this.h=15
-        this.x=width/2-this.w/2
-        this.y=height - this.h-10
-        this.dir=0
+  class Jugador {
+    constructor() {
+        this.w = 100
+        this.h = 15
+        this.x = width / 2 - this.w / 2
+        this.y = height - this.h - 10
+        this.dir = 0
     }
 
-    move(dir){
-        this.dir=dir
+    move(dir) {
+        this.dir = dir
     }
 
-    update(){
-        this.x += this.dir*7
-        this.xs = constrain(this.x,0,width-this.w)
+    update() {
+        this.x += this.dir * 7
+        this.x = constrain(this.x, 0, width - this.w)
     }
 
-    mostrar(){
-        fill("red")
+    mostrar() {
+        fill(255, 255, 255)
         rect(this.x, this.y, this.w, this.h)
     }
   }
 
-  class Bola{
-    constructor(){
+  class Bola {
+    constructor() {
         this.reset()
+        this.lanzada = false
     }
 
-    reset(){
+    reset() {
         this.x = width / 2;
         this.y = height / 2;
         this.r = 10;
-        this.xspeed = velBola * (random()<0.5?1:-1)
-        this.yspeed = -velBola
+        this.xspeed = velBola * (random() < 0.5 ? 1 : -1)
+        this.yspeed = - velBola
+        this.lanzada = false
     }
 
-    update(){
+    update() {
+        if (!this.lanzada) return
+
         this.x += this.xspeed
         this.y += this.yspeed
 
-        if(this.x<0 || this.x> width) this.xspeed *=-1
-        if(this.y<0) this.yspeed *= -1
+        if (this.x < 0 || this.x > width) this.xspeed *= -1
+        if (this.y < 0) this.yspeed *= -1
     }
 
-    verificarJugador(pos){
-        if(this.y + this.r > pos.y && this.x > pos.x && this.x < pos.x + pos.w){
-            this.yspeed*=-1
-            this.y = pos.y - this.r
+    verificarJugador(jugador) {
+        if (this.y + this.r > jugador.y && this.x > jugador.x && this.x < jugador.x + jugador.w) {
+            this.y = jugador.y - this.r;
+            this.yspeed *= -1;
+    
+            let centroJugador = jugador.x + jugador.w / 2;
+            let distanciaDesdeCentro = (this.x - centroJugador) / (jugador.w / 2);
+    
+            this.xspeed = distanciaDesdeCentro * velBola;
         }
     }
 
-    suelo(){
-        if(this.y > height){
+    suelo() {
+        if (this.y > height) {
             vidas--
             this.reset()
         }
     }
 
-    golpe(cuadros){
-        if(this.x > cuadros.x && this.x < cuadros.x + cuadros.w && this.y > cuadros.y && this.y <cuadros.y+cuadros.h){
+    golpe(cuadros) {
+        if (this.x > cuadros.x && this.x < cuadros.x + cuadros.w && this.y > cuadros.y && this.y <cuadros.y+cuadros.h) {
             return true
         }
         return false
     }
 
-    reverseY(){
+    reverseY() {
         this.yspeed *=-1
     }
 
-    mostrar(){
-        fill(255, 204, 0);
-    ellipse(this.x, this.y, this.r * 2);
+    mostrar() {
+        fill(255, 255, 0);
+        ellipse(this.x, this.y, this.r * 2);
     }
   }
 
-  class Cuadro{
+  class Cuadro {
     constructor(x, y, w, h, golpe, irrompible) {
         this.x = x;
         this.y = y;
@@ -207,35 +232,45 @@ function setup() {
         this.irrompible = irrompible;
       }
 
-      mostrar(){
-        if(this.irrompible) fill(150)
-            else if(this.golpe === 3) fill(255,0,0)
-        else if(this.golpe === 2) fill(255,165,0)
-            else fill(0,255,0)
+      mostrar() {
+        if (this.irrompible) fill(58, 122, 154)
+            else if (this.golpe == 3) fill (239, 171, 98)
+        else if (this.golpe == 2) fill (228, 74, 42)
+            else fill(255, 255, 255)
         rect(this.x, this.y, this.w, this.h)
       }
   }
 
-  function getInfoNivel(niveles){
-    if(niveles === 1){
+  //aqui se accede a los niveles dependiendo en cual este y el mapa cambia
+  function getInfoNivel(niveles) {
+    if (niveles == 1) {
         return [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
-          ];
-    }else if (niveles === 2){
+        ];
+    } else if (niveles == 2) {
         return [
-            [1, 1, 2, 2, 1, 1, 1, 1, 2, 2],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 2, 2, 1, 1, 1, 1, 2, 2, 1],
             [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-          ]; 
-    }else if(niveles === 3){
+        ]; 
+    } else if (niveles == 3) {
         return [
-      [2, 2, 2, 3, 3, 3, 2, 2, 2, 2],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 2, 1, 0, 0, 0, 1, 2, 1, 1],
-      [0, 0, 1, 1, 1, 1, 1, 1, 0, 0]
-    ];
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
+            [0, 0, 1, 1, 3, 3, 1, 1, 0, 0]
+        ];
     }
     return []
   }
 
+  //Efecto para el diseño del juego
+  function efectoRetro() {
+    stroke(0, 0, 0, 50)
+    for (let y = 0; y < height; y += 6) {
+      line(0, y, width, y)
+      line(0, y + 1, width, y + 1)
+      line(0, y + 2, width, y + 2)
+    }
+  }
